8e15bfbeb6e3a5ec24d92aa6c81d0474c894fac4
hannahhan153 <hannah.han@biola.edu>
2022-02-18 19:43:02 -0800

    test and build base enemy constructor

---
+++ __tests__/Enemy.test.js
@@ -0,0 +1,17 @@
+// import and mock the necessary modules 
+const Enemy = require('../lib/Enemy.js');
+const Potion = require('../lib/Potion.js');
+
+jest.mock('../lib/Potion.js');
+
+test('creates enemy object', () => {
+    const Enemy = new Enemy('goblin', 'sword');
+
+    expect(enemy.name).toBe('goblin');
+    expect(enemy.weapon).toBe('sword');
+
+    expect(enemy.health).toEqual(expect.any(Number));
+    expect(enemy.strength).toEqual(expect.any(Number));
+    expect(enemy.agility).toEqual(expect.any(Number));
+    expect(enemy.potion).toEqual(expect.any(Object));
+});
